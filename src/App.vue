2ab0a6d296107f010620f75f478ce4a1258670ad
notngan <notngan@gmail.com>
2018-09-27 16:59:43 +0700

    getting started

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+  li {
+    list-style-type: none;
+  }
+  a {
+    color: black;
+    text-decoration: none;
+  }
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+  <div class="container">
+    <div class="header-logo">
+    </div>
+    <nav>
+      <ul id="menu">
+        <li v-for="menu in menus" :key="menu">
+          <a href="#"> {{ menu }} </a>
+        </li>
+      </ul>
+      <ul id="sub-menu">
+        <li v-for="icon in icons" :key="icon"><a href="#"><v-icon :name="icon" /></a></li>
+      </ul>
+    </nav>
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menus: [ 'home', 'rentals', 'services', 'about us', 'contact'],
+      icons: ['shopping-bag', 'user', 'phone']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+//variables
+$amber: #ffbf00;
+header {
+  height: 100px;
+  background: $amber;
+  * {
+    margin: 0;
+  }
+}
+</style>
